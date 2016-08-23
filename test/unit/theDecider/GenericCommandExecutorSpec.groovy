c7b8288205b5d8b8153cfb3166f7e3fd74ce39a0
Mark Waller <M_P_waller@hotmail.com>
2016-08-23 14:19:10 +0800

    Update GenericCommandExecutorSpec.groovy

--- test/unit/theDecider/GenericCommandExecutorSpec.groovy
+++ test/unit/theDecider/GenericCommandExecutorSpec.groovy
@@ -4,11 +4,12 @@ import grails.test.mixin.TestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
 import spock.lang.Specification
 import theDecider.GenericCommandExecutor;
-
+import spock.lang.Ignore
 /**
  * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
  */
 @TestMixin(GrailsUnitTestMixin)
+@Ignore
 class GenericCommandExecutorSpec extends Specification {
 	
 	GenericCommandExecutor cmdExec = new GenericCommandExecutor()
